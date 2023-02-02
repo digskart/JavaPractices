6bc49d4c7eb64b2a5dad787c5a03f7f9cf368dc8
suvajitroyofficial <suvajitroy66083@gmail.com>
2023-02-02 15:08:30 +0530

    Initial commit

---
+++ src/practiceclassjava.java
@@ -0,0 +1,17 @@
+public class practiceclassjava {
+
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 20;
+
+        int add = a+b;
+
+        if (add>71){
+            System.out.println("Okay");
+        } else {
+            System.out.println("Not Okay");
+        }
+
+    }
+
+}
